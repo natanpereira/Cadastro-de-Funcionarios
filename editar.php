cd9786a0008ebb45daff00dbdc2bb42b87767199
natan <natanj49@gmail.com>
2019-02-21 15:34:09 -0300

    atualizando projeto

--- editar.php
+++ editar.php
@@ -23,118 +23,115 @@ $mostraFunc = mostraFunc($id_func);
 
 <!DOCTYPE html>
 <html>
-<head>
-<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico"/>
-	<title>Editar Cadastro Funcionário</title>
-		<link rel="stylesheet" href="mdl/material.min.css">
-		<script src="mdl/material.min.js"></script>
-		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" />
-		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" ></script>
-		<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
-		<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.18/datatables.min.css"/>
-		<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
-  		<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>    
-		<link href="css/style.css" rel="stylesheet">
+	<head>
+	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico"/>
+		<title>Editar Cadastro Funcionário</title>
+			<link rel="stylesheet" href="mdl/material.min.css">
+			<script src="mdl/material.min.js"></script>
+			<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+			<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" />
+			<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" ></script>
+			<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+			<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.18/datatables.min.css"/>
+			<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
+	  		<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>    
+			<link href="css/style.css" rel="stylesheet">
 
-</head>
-<body background="images/fundo_linhas.jpg"> 
-<h2 align="center" >Editar Cadastro Funcionário</h2>
-<div class="container">
+	</head>
+	<body background="images/fundo_linhas.jpg"> 
+		<h2 align="center" >Editar Cadastro Funcionário</h2>
+		<div class="container">
+		<?php foreach($mostraFunc as $valor): ?> 
+				<form method ="post" action="editar.php">
+				<div class="form-group">
+					<label>Id</label>
+					<input type="text" name="id" class="form-control" readonly="true" value="<?php echo $valor['id_func'];?>" />
+				</div>
+				<div class="form-group">
+					<label>Nome</label>
+					<input type="text" name="nome" class="form-control" required="true" value="<?php echo $valor['nome_func'];?>"  />
+				</div>
 
-<?php foreach($mostraFunc as $valor): ?> 
+				<div class="form-group">
+					<label>CPF</label>
+					<input type="text" name="cpf" class="form-control" readonly="true" value="<?php echo $valor['cpf_func'];?>"  />
+				</div>
 
-		<form method ="post" action="editar.php">
-		<div class="form-group">
-			<label>Id</label>
-			<input type="text" name="id" class="form-control" readonly="true" value="<?php echo $valor['id_func'];?>" />
+				<div class="form-group">
+					<label>Data admissão</label>
+					<input type="text" name="data" class="form-control" readonly="true" value="<?php echo date('d/m/Y', strtotime($valor['data_admissao']));?>"  />
+				</div>
+
+				<div class="form-group">
+					<label>Cargo</label>
+					<select name= "cargo" class="form-control">
+						<option selected="true"><?php echo $valor['cargo'];?></option>
+						<option>Administrador De Dados</option>
+						<option>Analista Financeiro Jr</option>
+						<option>Analista De Negócios Pl</option>
+						<option>Analista De Rh Sênior</option>
+						<option>Analista De Suporte JR</option>
+						<option>Analista De Suporte Pl</option>
+						<option>Analista De Suporte Sr</option>
+						<option>Analista De Suporte</option>
+						<option>Analista De Testes Jr</option>
+						<option>Analista Financeiro Sr</option>
+						<option>Analista Programador Pl</option>
+						<option>Analista Programador Sênior</option>
+						<option>Analista Programador Sr</option>
+						<option>Assistente Administrativo Pl</option>
+						<option>Assistente Comercial</option>
+						<option>Assistente Financeiro Sr</option>
+						<option>Coordenador De Infraestrutura</option>
+						<option>Coordenador De Operações</option>
+						<option>Coordenador De Projetos</option>
+						<option>DBA PI</option>
+						<option>Dba Sr</option>
+						<option>Estagiário</option>
+						<option>Gerente Administrativa</option>
+						<option>Gerente Comercial</option>
+						<option>Gerente De Governança</option>
+						<option>Gerente De Projetos</option>
+						<option>Líder Técnico</option>
+						<option>PJ - Analista de Teste</option>
+						<option>PJ - CEO</option>
+						<option>PJ - Marketing</option>
+						<option>PJ</option>
+					</select>			
+				</div>
+				
+				<div class="form-group">
+					<label>Área</label>
+					<select name="area" class="form-control">
+						<option selected="true"><?php echo $valor['area'];?></option>
+						<option>Administrativo e Financeiro</option>
+						<option>Desenvolvimento e Manutenção de Sistemas</option>
+						<option>Governança Corporativa</option>
+						<option>Governança TI/Segurança da Informação</option>
+						<option>Informações Gerenciais</option>
+						<option>Infraestrutura</option>
+						<option>Inovação</option>
+						<option>Marketing</option>
+						<option>Negócios</option>
+						<option>Operações</option>
+						<option>Operações e Tecnologia</option>
+						<option>Pré-Vendas/Implatação</option>
+						<option>Projetos e relacionamento Internacional</option>
+						<option>Quality Assurance</option>
+						<option>Recursos Humanos</option>
+					</select>			
+				</div>
+
+				<div class="form-group">
+					<label>Salário</label>
+					<input type="text" name="salario" class="form-control" required="true" value="<?php echo $valor['salario'];?>"/>			
+				</div>	
+				<div class="form-group">
+					<button type="submit" class="btn btn-success" style="float: right;">Alterar</button>	
+				</div>
+			</form> 
+			<?php endforeach?>	
+			<a href="listafunc.php"><button type="submit" class="btn btn-primary">Voltar</button></a>
 		</div>
-		<div class="form-group">
-			<label>Nome</label>
-			<input type="text" name="nome" class="form-control" required="true" value="<?php echo $valor['nome_func'];?>"  />
-		</div>
-
-		<div class="form-group">
-			<label>CPF</label>
-			<input type="text" name="cpf" class="form-control" readonly="true" value="<?php echo $valor['cpf_func'];?>"  />
-		</div>
-
-		<div class="form-group">
-			<label>Data admissão</label>
-			<input type="text" name="data" class="form-control" readonly="true" value="<?php echo date('d/m/Y', strtotime($valor['data_admissao']));?>"  />
-		</div>
-
-		<div class="form-group">
-			<label>Cargo</label>
-			<select name= "cargo" class="form-control">
-				<option selected="true"><?php echo $valor['cargo'];?></option>
-				<option>Administrador De Dados</option>
-				<option>Analista Financeiro Jr</option>
-				<option>Analista De Negócios Pl</option>
-				<option>Analista De Rh Sênior</option>
-				<option>Analista De Suporte JR</option>
-				<option>Analista De Suporte Pl</option>
-				<option>Analista De Suporte Sr</option>
-				<option>Analista De Suporte</option>
-				<option>Analista De Testes Jr</option>
-				<option>Analista Financeiro Sr</option>
-				<option>Analista Programador Pl</option>
-				<option>Analista Programador Sênior</option>
-				<option>Analista Programador Sr</option>
-				<option>Assistente Administrativo Pl</option>
-				<option>Assistente Comercial</option>
-				<option>Assistente Financeiro Sr</option>
-				<option>Coordenador De Infraestrutura</option>
-				<option>Coordenador De Operações</option>
-				<option>Coordenador De Projetos</option>
-				<option>DBA PI</option>
-				<option>Dba Sr</option>
-				<option>Estagiário</option>
-				<option>Gerente Administrativa</option>
-				<option>Gerente Comercial</option>
-				<option>Gerente De Governança</option>
-				<option>Gerente De Projetos</option>
-				<option>Líder Técnico</option>
-				<option>PJ - Analista de Teste</option>
-				<option>PJ - CEO</option>
-				<option>PJ - Marketing</option>
-				<option>PJ</option>
-			</select>			
-		</div>
-		
-		<div class="form-group">
-			<label>Área</label>
-			<select name="area" class="form-control">
-				<option selected="true"><?php echo $valor['area'];?></option>
-				<option>Administrativo e Financeiro</option>
-				<option>Desenvolvimento e Manutenção de Sistemas</option>
-				<option>Governança Corporativa</option>
-				<option>Governança TI/Segurança da Informação</option>
-				<option>Informações Gerenciais</option>
-				<option>Infraestrutura</option>
-				<option>Inovação</option>
-				<option>Marketing</option>
-				<option>Negócios</option>
-				<option>Operações</option>
-				<option>Operações e Tecnologia</option>
-				<option>Pré-Vendas/Implatação</option>
-				<option>Projetos e relacionamento Internacional</option>
-				<option>Quality Assurance</option>
-				<option>Recursos Humanos</option>
-			</select>			
-		</div>
-
-		<div class="form-group">
-			<label>Salário</label>
-			<input type="text" name="salario" class="form-control" required="true" value="<?php echo $valor['salario'];?>"  />			
-		</div>	
-		<div class="form-group">
-			<button type="submit" class="btn btn-success" style="float: right;">Alterar</button>	
-		</div>
-	</form> 
-	<?php endforeach?>	
-	<a href="listafunc.php"><button type="submit" class="btn btn-primary">Voltar</button></a>
-</div>
-
-</body>
+	</body>
 </html>
--- index.php
+++ index.php
@@ -4,34 +4,33 @@ header('Content-type: text/html; charset=utf-8');
 
 <!DOCTYPE html>
 <html>
-<head>
-<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico"/>
-	<title>Página Inicial</title>
-	<link rel="stylesheet" href="https://cdn.datatable.net/1.10.19/css/jquery.dataTables.min.css">
-	<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-	<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" />
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" ></script>
-	<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.18/datatables.min.css"/>
-	<script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.18/datatables.min.js"></script>
-	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
-	<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
-</head>
-<body background="images/fundo_linhas.jpg">
-<header>
-<nav>
-<h1 align="center">Funcionários Duosystem</h1>
-	<ul>
-	<div>
-		<a href="listafunc.php">Listar Funcionários</a>
-		</div>
-		<br>
-		<div>
-		<a href="novo.php">Novo Funcionário</a>
-		</div>
-	</ul>
-</nav>
-</header>
-</body>
+		<head>
+			<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico"/>
+			<title>Página Inicial</title>
+				<link rel="stylesheet" href="https://cdn.datatable.net/1.10.19/css/jquery.dataTables.min.css">
+				<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+				<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+				<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" />
+				<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" ></script>
+				<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+				<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.18/datatables.min.css"/>
+				<script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.18/datatables.min.js"></script>
+				<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
+				<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
+		</head>
+	<body background="images/fundo_linhas.jpg">
+		<header>
+			<div>
+				<div>
+				<h1 align="center" class="font-weight-bold">Funcionários Duosystem</h1>
+				</div>
+			<div style="text-align: center;"> 
+				<div class="btn-group" role="group" aria-label="Basic example">
+					<a href="listafunc.php"><button type="button" class="btn btn-primary" >Listar Funcionários</button></a>
+					<a href="novo.php"><button type="button" class="btn btn-primary">Novo Funcionário</button></a>	
+				</div>		
+			</div>
+			</div>
+		</header>
+	</body>
 </html>
